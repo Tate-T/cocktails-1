29f32256350ae891a67e7ceeb8af1cc5f24e4377
Pan-Artur <pankovets.artur1@gmail.com>
2025-02-15 17:17:33 +0200

    addition js for hero

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import 'normalize.css';
 
 // import "./js/components/header";
-// import "./js/partials/hero";
+import "./js/partials/hero/main-hero.js";
 // import "./js/partials/error-section";
 // import "./js/partials/favorite-cocktails";
