598275b5b35f4c3f2a69fc92c402afd23aaad148
Maksym Ryaboshapka <e.ryaboshapka777@gmail.com>
2025-02-22 18:39:58 +0200

    add favorite beta

--- src/favorite.js
+++ src/favorite.js
@@ -2,4 +2,5 @@ import "normalize.css";
 
 import "./js/partials/favorite-cocktails/like-event";
 import "./js/components/modal/modal";
-import "./js/partials/favorite-cocktails/render-favorite";
+// import "./js/partials/favorite-cocktails/render-favorite";
+import "./js/partials/favorite-cocktails/render-from-local-storage"
---
+++ src/js/partials/favorite-cocktails/render-from-local-storage.js
@@ -0,0 +1,85 @@
+import axios from "axios";
+import _ from "lodash";
+
+const favoriteDrinks = [];
+
+const favoriteDrinksLocalStorage = JSON.parse(
+  localStorage.getItem("favorite-cocktails"),
+);
+
+if (favoriteDrinksLocalStorage !== null) {
+  if (favoriteDrinksLocalStorage.length !== 0) {
+    favoriteDrinksLocalStorage.forEach(cocktail => {
+      favoriteDrinks.push(cocktail);
+    });
+
+    const list = document.querySelector(".cocktails");
+
+    favoriteDrinksLocalStorage.forEach(cocktail => {
+      const html = `
+      <li class="cocktails__item" id="${cocktail.idDrink}">
+        <img
+          src="${cocktail.strDrinkThumb}"
+          alt="#"
+          class="cocktails__img"
+        />
+
+        <div class="cocktails__padding">
+          <h3 class="cocktails__subtitle">${cocktail.strDrink}</h3>
+          <div class="cocktails__box">
+            <button class="cocktails__learn-more">Learn more</button>
+            <button class="cocktails__add-favorite cocktails__add-favorite--styles">
+              <span>Add to</span>
+              <svg class="cocktails__icon">
+                <use
+                  href="../../assets/icons/symbol-defs.svg#icon-desktop-heart"
+                ></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </li>
+    `;
+
+      list.insertAdjacentHTML("beforeend", html);
+    });
+
+    console.log(favoriteDrinks);
+
+    
+    document.addEventListener("click", async e => {
+      setTimeout(async () => {
+        // Add
+        if (e.target.classList.contains("cocktails__remove-favorite")) {
+          const id = e.target.closest("li").id;
+    
+          await axios
+            .get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+            .then(res => favoriteDrinks.push(res.data.drinks[0]));
+    
+          localStorage.setItem(
+            "favorite-cocktails",
+            JSON.stringify(favoriteDrinks),
+          );
+        }
+    
+        // Remove
+        else if (e.target.classList.contains("cocktails__add-favorite")) {
+          const id = e.target.closest("li").id;
+    
+          const currCocktail = await axios
+            .get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+            .then(res => res.data.drinks[0]);
+    
+          _.remove(favoriteDrinks, currCocktail);
+    
+          localStorage.setItem(
+            "favorite-cocktails",
+            JSON.stringify(favoriteDrinks),
+          );
+        }
+      }, 500);
+    });
+    
+  }
+}
