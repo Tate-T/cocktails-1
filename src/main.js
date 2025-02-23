02d2e529d4887496be34996d8aaa915d1682c772
Maksym Ryaboshapka <e.ryaboshapka777@gmail.com>
2025-02-23 14:16:13 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import "normalize.css";
 
-import "./js/components/header/header.js";
-import "./js/partials/main-cocktails/main-cocktails.js";
+import "./js/components/header/header";
+import "./js/partials/main-cocktails/main-cocktails";
 import "./js/components/modal/modal";
+import "./js/partials/cocktails/render-icons-local-storage";
+import "./js/partials/cocktails/unique-cocktails";
 // import "./js/partials/hero/main-hero.js";
 // import "./js/partials/favorite-cocktails/favorite-cocktails-managment/like-event.js";
 // import "./js/partials/main-cocktails/render-cocktails.js";
