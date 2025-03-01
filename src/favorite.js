643a724d0c46976bdfaa4f64389787fc5091047f
Maksym Ryaboshapka <e.ryaboshapka777@gmail.com>
2025-03-01 13:53:34 +0200

    fix unique

--- src/favorite.js
+++ src/favorite.js
@@ -3,4 +3,4 @@ import "normalize.css";
 import "./js/components/header/header.js";
 import "./js/partials/favorite-cocktails/favorite-cocktails.js";
 import "./js/components/modal/modal";
-import "./js/partials/cocktails/unique-cocktails";
+import "./js/partials/favorite-cocktails/test-on-unique";
