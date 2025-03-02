fe9a09c64d6c0c5d1bc816aab9c69178375be344
kleryxx <126974469+Vlad-Lipinskyi@users.noreply.github.com>
2025-03-02 12:47:34 -0500

    fix

--- src/js/components/ingredients-modal/ingredient-modal.js
+++ src/js/components/ingredients-modal/ingredient-modal.js
@@ -1,5 +1,7 @@
 import { openIngModal } from "./switch-modal/open-ingredients-modal";
 import { closeIngModal } from "./switch-modal/close-ingredients-modal";
 
-openIngModal();
-closeIngModal();
+document.addEventListener("DOMContentLoaded", () => {
+  openIngModal();
+  closeIngModal();
+});
---
+++ src/js/services/get-ingredients-info-api.js
@@ -0,0 +1,14 @@
+// export const fetchIngredientInfo = async (ingredientId) => {
+//     const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${ingredientId}`;
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     return data.ingredients[0];  
+//   };
+  
+//   export const fetchCocktailInfo = async (cocktailId) => {
+//     const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`;
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     return data.drinks[0]; 
+//   };
+  
